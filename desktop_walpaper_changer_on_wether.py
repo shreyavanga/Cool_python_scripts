f3df40bfae5f18acd0fcd8105a6f6930b7476fb5
Ankush <ankush.mitra@students.iiit.ac.in>
2019-10-25 16:15:39 +0530

    making webscrapping for weather

---
+++ desktop_walpaper_changer_on_wether.py
@@ -0,0 +1,23 @@
+# www.timeanddate.com/weather/india/
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def weather(city):
+    try:
+        the_url = "https://www.timeanddate.com/weather/india/" + city
+        the_page = urllib.request.urlopen(the_url)
+    except Exception as e:
+        return "Invaild city name"
+
+    soup = BeautifulSoup(the_page, "html.parser")
+    content = soup.findAll('div', {"class": "three columns"})[0]
+    temp = content.find('div', {'class': 'h2'}).text
+    cond = str(content.findAll('p')[0].text)
+    degree = temp.split(" ")[0].encode('ascii', 'ignore').decode()
+    msg = degree + " - " + cond
+    return msg
+def main():
+    read city
+    msg=weather(city)
+    echo (msg)
