7d24d8adf59c6a5895afa75b410bbc5f96a0149c
Ankush <ankush.mitra@students.iiit.ac.in>
2019-10-27 17:27:59 +0530

    finally commit on desktop_walpaper_change_on_weather.py

--- desktop_walpaper_changer_on_wether.py
+++ desktop_walpaper_changer_on_wether.py
@@ -2,8 +2,8 @@
 import urllib
 from urllib import request
 from bs4 import BeautifulSoup
-
-
+import subprocess
+import datetime
 def weather(city):
     try:
         the_url = "https://www.timeanddate.com/weather/india/" + city
@@ -16,9 +16,68 @@ def weather(city):
     temp = content.find('div', {'class': 'h2'}).text
     cond = str(content.findAll('p')[0].text)
     degree = temp.split(" ")[0].encode('ascii', 'ignore').decode()
-    msg = degree + " - " + cond
-    return msg
 
-city=input()
-msg=weather(city)
-print(msg)
+    return degree,cond
+
+city = input()
+degree,cond = weather(city)
+t_degree=degree[0:len(degree)-1]
+act=int(t_degree)
+hourss = datetime.datetime.now()
+hours= hourss.hour
+print ("current time=",hours,"cureent_temp=",act,"current_weather=",cond)
+if act<10 and hours<=18:
+    subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format(r'image/day_cold.jpg'), shell=True)
+if act<10 and hours>18:
+    subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format(r'image/night_cold.jpg'), shell=True)
+su_cloud='cloud'
+su_sun='sunny'
+su_fog='fog'
+su_rain='rain'
+su_thunder='thunder'
+su_storm='storm'
+cl=cond.find(su_cloud)
+su=cond.find(su_sun)
+ra=cond.find(su_rain)
+fo=cond.find(su_fog)
+th=cond.find(su_thunder)
+st=cond.find(su_storm)
+if act>10 and hours<=18:
+    if cl != -1:
+        print("in line 47")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/day_clould.jpg"), shell=True)
+    elif su !=-1:
+        print("in line 50")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/day_sun.jpg"), shell=True)
+    elif ra !=-1:
+        print("in line 53")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/rain.jpg"), shell=True)
+    elif th!=-1:
+        print("in line 56")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/thunder.jpg"), shell=True)
+    elif st!=-1:
+        print("in line 59")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/thunder.jpg"), shell=True)
+    elif fo!=-1:
+        print("in line 62")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/fog.jpg"), shell=True)
+
+elif act>10 and hours>18:
+    if cl != -1:
+        print("in line 47")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/night_cloud.jpg"), shell=True)
+    elif su !=-1:
+        print("in line 50")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/night_moon.jpg"), shell=True)
+    elif ra !=-1:
+        print("in line 53")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/night_rain.jpg"), shell=True)
+    elif th!=-1:
+        print("in line 56")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/thunder.jpg"), shell=True)
+    elif st!=-1:
+        print("in line 59")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/thunder.jpg"), shell=True)
+    elif fo!=-1:
+        print("in line 62")
+        subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/night_cold.jpg"), shell=True)
--- webs.py
+++ webs.py
@@ -1,12 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-def trader_spider(max_pages):
-    page=1
-    while page<max_pages:
-        url='https://buckysroom.org/trade/search.php?page='+str(page)
-        source_code=requests.get(url)
-        pal_text=source_code.text
-        soup=BeautifulSoup(pal_text)
-
-
+import subprocess
+subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format("/home/ankush/cool_python_scripts/image/day_sun.jpg"), shell=True)
